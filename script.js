119bead65f1f71e8d70209a37b796a12d3f9f583
SimpleX <118108084+SimpleX-T@users.noreply.github.com>
2023-10-08 19:09:12 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-/* ========================================== typing animation =========================================== */
-var typed = new Typed(".typing", {
-    strings:["", "Web Designer", "Web Developer", "Graphic Desiner", "YouTuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
